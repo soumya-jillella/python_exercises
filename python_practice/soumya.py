ccee447e3c8979165158c53c7ba5e418bc7ee059
soumyareddylinux <soumya98.j@gmail.com>
2018-09-11 13:03:01 +0530

    using self print the names

---
+++ python_practice/soumya.py
@@ -0,0 +1,19 @@
+#f = open("test.txt")      
+#f = open("test.txt",'w') 
+#f = open("img.bmp",'r+b') 
+#def file():
+
+	#try:
+   		
+  		#file = open("test.txt",encoding = 'utf-8')
+   # perform file operations
+	#finally:
+   		#file.close()
+   # perform file operations
+	
+#f = open("soumya.txt","w")    
+#f = open("C:/Users/admin/Desktop/python_exercises/python_practice")  
+
+f = open("soumyafile.txt","w")
+f = open("C:/Users/admin/Desktop/python_exercises/python_practice/soumyafile.txt")
+f.close()
